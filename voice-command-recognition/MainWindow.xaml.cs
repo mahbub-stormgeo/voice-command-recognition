d68306bf529eac1b07eeef391a4f707f06e0a8d9
mahbub-stormgeo <Mahbubur.Rahman@stormgeo.com>
2018-12-18 12:21:32 +0600

    update grammer.

--- voice-command-recognition/MainWindow.xaml.cs
+++ voice-command-recognition/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Speech.Recognition;
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Speech.Recognition;
 using System.Windows;
 
 namespace voice_command_recognition
@@ -8,13 +10,14 @@ namespace voice_command_recognition
     /// </summary>
     public partial class MainWindow : Window
     {
-        SpeechRecognitionEngine speechRecognizer = new SpeechRecognitionEngine();
+        SpeechRecognitionEngine speechRecognizer = new SpeechRecognitionEngine(new CultureInfo("en-us"));
         public MainWindow()
         {
             InitializeComponent();
-            speechRecognizer.SpeechRecognized += speechRecognizer_SpeechRecognized;
-            speechRecognizer.LoadGrammar(GetGrammar());
-            speechRecognizer.SetInputToDefaultAudioDevice();            
+            speechRecognizer.SetInputToDefaultAudioDevice();
+            speechRecognizer.SpeechRecognized += speechRecognizer_SpeechRecognized; 
+            speechRecognizer.LoadGrammarAsync(GetGrammarForZooming());
+            speechRecognizer.LoadGrammarAsync(GetMathGrammer());
         }
         private void btnStartListen_Click(object sender, RoutedEventArgs e)
         {
@@ -22,12 +25,25 @@ namespace voice_command_recognition
             btnStartListen.Content = "Listening started...";
             btnStartListen.IsEnabled = false;
             btnStopListen.IsEnabled = true;
-            txtSpeech.Text = "";
-            speechRecognizer.RecognizeAsync(RecognizeMode.Single);
+            txtSpeech.Text = "welcome to naviplanner...";
+            speechRecognizer.RecognizeAsync(RecognizeMode.Multiple);
         }
         private void speechRecognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
-            txtSpeech.Text = txtSpeech.Text + " \n" + e.Result.Text;
+            string txt = e.Result.Text;
+            float confidence = e.Result.Confidence;
+            Debug.WriteLine(string.Format("Recognized {0} with confidence {1}", txt, confidence));
+            if (confidence < 0.60) return;
+            if (txt.IndexOf("naviplanner") >= 0 && txt.IndexOf("zoom in") > 0)
+            {
+                txtSpeech.FontSize--;
+                Debug.WriteLine(txtSpeech.FontSize.ToString());
+            }
+            if (txt.IndexOf("naviplanner") >= 0 && txt.IndexOf("zoom out") > 0)
+            {
+                txtSpeech.FontSize++;
+                Debug.WriteLine(txtSpeech.FontSize.ToString());
+            }
         }
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
@@ -54,6 +70,30 @@ namespace voice_command_recognition
             grammarBuilder.Append(valueChoices);
 
             return new Grammar(grammarBuilder);
+        }
+        private Grammar GetGrammarForZooming()
+        {
+            GrammarBuilder grammarBuilder = new GrammarBuilder();
+            grammarBuilder.Append("Hey NaviPlanner");
+            Choices valueChoices = new Choices();
+            valueChoices.Add("zoom in");
+            valueChoices.Add("zoom out");
+            grammarBuilder.Append(valueChoices);
+            return new Grammar(grammarBuilder);
+        }
+        private Grammar GetMathGrammer()
+        {
+            Choices ch_Numbers = new Choices();
+            ch_Numbers.Add("1");
+            ch_Numbers.Add("2");
+            ch_Numbers.Add("3");
+            ch_Numbers.Add("4");
+            GrammarBuilder gb_WhatIsXplusY = new GrammarBuilder();
+            gb_WhatIsXplusY.Append("What is");
+            gb_WhatIsXplusY.Append(ch_Numbers);
+            gb_WhatIsXplusY.Append("plus");
+            gb_WhatIsXplusY.Append(ch_Numbers);
+            return new Grammar(gb_WhatIsXplusY);
         }
     }
 }
